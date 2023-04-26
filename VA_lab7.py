6d465c40b5da8ef72890560ccf7179d3b51458c0
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-04-26 17:30:35 +0700

    Добавьте файлы проекта.

---
+++ VA_lab7.py
@@ -0,0 +1,15 @@
+print('a = ', end = '')
+a = float(input())
+print('\n')
+
+print('b = ', end = '')
+b = float(input())
+print('\n')
+
+while True:
+    print('R = ', end = '')
+    R = float(input())
+    print('\n')
+
+    if R > 0:
+        break
