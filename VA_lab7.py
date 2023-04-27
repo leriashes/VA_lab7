6bca362a116fa9776bd317eab086467be1e62baa
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-04-28 00:13:17 +0700

    Графики

--- VA_lab7.py
+++ VA_lab7.py
@@ -1,3 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import getch
+
 def F1(x):
     return pow(x[0], 2) + pow(x[1], 2) / 4 - 1
 
@@ -13,16 +18,15 @@ def f1y(y):
 def f2(x, a):
     return 2 * (x - a)
 
-def Gauss(matrix, b):
+def count(matrix, b):
 	p = 0
-
-	#поиск главного элемента в столбце (наибольшего по модулю)
+	
 	if (abs(matrix[1][0]) > abs(matrix[0][0])):
 		(matrix[1], matrix[0]) = (matrix[0], matrix[1])
 		(b[1], b[0]) = (b[0], b[1])
 		p = 1
+		
 
-	#прямой ход
 	coef = 0;
 
 	if matrix[0][0] != 0:
@@ -34,18 +38,17 @@ def Gauss(matrix, b):
 	b[1] -= b[0] * coef
 
 
-
 	det = pow(-1, p)
 
 	for i in range(2):
 		det *= matrix[i][i]
 
+	x = [0, 0]
 
 	if det == 0:
 		print("Ошибка! Матрица вырожденная!")
+		exit()
 	else:
-		x = [0, 0]
-
 		for i in range(1, -1, -1):
 			s = 0
 
@@ -54,54 +57,110 @@ def Gauss(matrix, b):
 
 			x[i] = (b[i] - s) / matrix[i][i]
 
-	return x;
+	return x
 
 
 
 
 
-
-
-
-print('a = ', end = '')
-a = float(input())
-
-print('b = ', end = '')
-b = float(input())
 
 while True:
-    print('R = ', end = '')
-    R = float(input())
-    print('\n')
+	
+	print('a = ', end = '')
+	a = float(input())
 
-    if R > 0:
-        break
+	print('b = ', end = '')
+	b = float(input())
 
-p = [0, 0]
+	while True:
+		print('R = ', end = '')
+		R = float(input())
+		print('\n')
 
-print('Начальное приближение\nx = ', end = '')
-p[0] = float(input())
+		if R > 0:
+			break
 
-print('y = ', end = '')
-p[1] = float(input())
-print('\n')
+	p = [0, 0]
 
-t = 0.00000000001
+	h = 0.001
+	xs = np.arange(-1, 1, h)
+	plt.plot(xs, 2 * np.sqrt(1 - xs * xs), 'b')
+	plt.plot(xs, (-2) * np.sqrt(1 - xs * xs), 'b')
+	plt.plot([xs.max(), 1, xs.max()], [2 * np.sqrt(1 - xs.max() * xs.max()), 0, -2 * np.sqrt(1 - xs.max() * xs.max())], 'b')
 
-delta = [0, 0]
+	g1 = a - R
+	g2 = a + R
 
-while True:
-	F = [F1(p) * (-1), F2(p, a, b, R) * (-1)]
-	f = [[f1x(p[0]), f1y(p[1])], [f2(p[0], a), f2(p[1], b)]]
-	delta = Gauss(f, F)
+	xs = np.arange(g1, g2, h)
+	plt.plot(xs, b + np.sqrt(R * R - pow(xs - a, 2)), 'r')
+	plt.plot(xs, b - np.sqrt(R * R - pow(xs - a, 2)), 'r')
+	plt.plot([xs.max(), g2, xs.max()], [b + np.sqrt(R * R - pow(xs.max() - a, 2)), b, b - np.sqrt(R * R - pow(xs.max() - a, 2))], 'r')
 
-	print(p[0], ' ', p[1])
-	print(delta[0], ' ', delta[1], '\n')
+	plt.grid(True)
+	plt.show()
 
-	for i in range(2):
-		p[i] = p[i] + delta[i]
+	print('Начальное приближение\nx = ', end = '')
+	p[0] = float(input())
 
-	if abs(delta[0]) < t and abs(delta[1]) < t:
+	print('y = ', end = '')
+	p[1] = float(input())
+	print('\n')
+
+	print('Точность вычислений: ', end = '')
+	t = 0.00000000001
+	#t = float(input())
+	print('\n')
+
+	delta = [0, 0]
+	k = 0
+
+	xs = np.arange(-1, 1, h)
+	plt.plot(xs, 2 * np.sqrt(1 - xs * xs), 'b')
+	plt.plot(xs, (-2) * np.sqrt(1 - xs * xs), 'b')
+	plt.plot([xs.max(), 1, xs.max()], [2 * np.sqrt(1 - xs.max() * xs.max()), 0, -2 * np.sqrt(1 - xs.max() * xs.max())], 'b')
+
+	xs = np.arange(g1, g2, h)
+	plt.plot(xs, b + np.sqrt(R * R - pow(xs - a, 2)), 'r')
+	plt.plot(xs, b - np.sqrt(R * R - pow(xs - a, 2)), 'r')
+	plt.plot([xs.max(), g2, xs.max()], [b + np.sqrt(R * R - pow(xs.max() - a, 2)), b, b - np.sqrt(R * R - pow(xs.max() - a, 2))], 'r')
+
+
+	while k < 1000:
+		F = [F1(p) * (-1), F2(p, a, b, R) * (-1)]
+		f = [[f1x(p[0]), f1y(p[1])], [f2(p[0], a), f2(p[1], b)]]
+		delta = count(f, F)
+
+		plt.plot(p[0], p[1], 'go')
+		plt.plot([p[0], p[0] + delta[0]], [p[1], p[1] + delta[1]], 'g')
+
+		#print(p[0], ' ', p[1])
+		#print(delta[0], ' ', delta[1], '\n')
+
+		for i in range(2):
+			p[i] = p[i] + delta[i]
+
+		if abs(delta[0]) < t and abs(delta[1]) < t:
+			break
+
+		k += 1
+
+	if k < 1000:
+		print('Результат x = ', p[0], '    y = ', p[1])
+
+		print('\n')
+
+		plt.grid(True)
+		plt.show()
+
+	else:
+		print('Решение расходится!')
+		plt.show()
+
+	print('\nЧтобы продолжить нажмите Enter. Для выхода из программы нажмите любую другую клавишу. ', end='\n')
+	cont = getch.getch()
+   
+	if cont == '\n' or cont == b'\r':
+		print('\n\n')
+	else:
 		break
-
-print('Результат x = ', p[0], '    y = ', p[1])
+    
